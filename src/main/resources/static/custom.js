6a460c3962bb3ad5f10aae242b3896653df875d5
anupama <anupama_avula@yahoo.com>
2020-08-04 23:07:33 -0400

    Second commit

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,3 +1,71 @@
+// var map, infoWindow, busInfo;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: {
+//       lat: parseFloat(busLocations[0].LATITUDE),
+//       lng: parseFloat(busLocations[0].LONGITUDE),
+//     },
+//     zoom: 15,
+//     scrollwheel: false,
+//   });
+//   var iconBase = "http://maps.google.com/mapfiles/kml/shapes/";
+//   for (let i = 0; i < busLocations.length; i++) {
+//     var marker = new google.maps.Marker({
+//       position: {
+//         lat: parseFloat(busLocations[i].LATITUDE),
+//         lng: parseFloat(busLocations[i].LONGITUDE),
+//       },
+//       map: map,
+//       icon: iconBase + "bus.png",
+//     });
+//   }
+//   // transit map layer
+//   const transitLayer = new google.maps.TransitLayer();
+//   transitLayer.setMap(map);
+//   infoWindow = new google.maps.InfoWindow();
+//   // Try HTML5 geolocation.
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(
+//       function (position) {
+//         var pos = {
+//           lat: position.coords.latitude,
+//           lng: position.coords.longitude,
+//         };
+//         infoWindow.setPosition(pos);
+//         infoWindow.setContent("You are here.");
+//         infoWindow.open(map);
+//         map.setCenter(pos);
+//       },
+//       function () {
+//         handleLocationError(true, infoWindow, map.getCenter());
+//       }
+//     );
+//   } else {
+//     // Browser doesn't support Geolocation
+//     handleLocationError(false, infoWindow, map.getCenter());
+//   }
+// }
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//   infoWindow.setPosition(pos);
+//   infoWindow.setContent(
+//     browserHasGeolocation
+//       ? "Error: The Geolocation service failed."
+//       : "Error: Your browser doesn't support geolocation."
+//   );
+//   infoWindow.open(map);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
 let map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -5,11 +73,35 @@ function initMap() {
         zoom: 15,
         scrollwheel: false,
     });
-	
-    for (i=0; i<busLocations.length; i++){
-        var marker = new google.maps.Marker({
-            position: { lat: parseFloat(busLocations[i].LATITUDE), lng: parseFloat(busLocations[i].LONGITUDE) },
-            map: map,
-        })
-    }
+for (i = 0; i < busLocations.length; i++) {
+    var marker = new google.maps.Marker({
+      position: {
+        lat: parseFloat(busLocations[i].LATITUDE),
+        lng: parseFloat(busLocations[i].LONGITUDE),
+      },
+      map: map,
+      icon: {
+        url:
+          "https://icon-library.com/images/google-maps-bus-icon/google-maps-bus-icon-14.jpg",
+        scaledSize: new google.maps.Size(32, 32), // pixels
+      },
+    });
+  }
 }
+	
+//    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
